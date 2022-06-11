505cd8ab16b88a91157437156d26bed5aea9613e
rasedul99 <rasedul2199@gmail.com>
2022-06-11 17:57:09 +0600

    Searchbar

---
+++ src/components/Searchbar.js
@@ -0,0 +1,26 @@
+const Searchbar = () => {
+  const [text, settext] = useState("");
+  const onChange = (q) => {
+    settext(q);
+  };
+  return (
+    <div class="navbar bg-[#F9FAFB]">
+      <h3 className="text-2xl mx-3">
+        <HiSearch />
+      </h3>
+      <form>
+        <input
+          type="text"
+          placeholder="Search Products"
+          class="input text-xl  bg-transparent w-full "
+          value={text}
+          onChange={(e) => onChange(e.target.value)}
+          autoFocus
+        />
+      </form>
+      
+    </div>
+  );
+};
+
+export default Searchbar;
---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#F4F6F8",
+
+          secondary: "#EEF0F9",
+
+          accent: "#37CDBE",
+
+          neutral: "#3D4451",
+
+          "base-100": "#FFFFFF",
+
+          info: "#3ABFF8",
+
+          success: "#36D399",
+
+          warning: "#FBBD23",
+
+          error: "#F87272",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
+
